79aa6e438e7e595218b82b80e987ed6a221cb5b8
Hedgar Bezerra <hedgarbezerra35@outlook.com>
2024-07-01 15:52:15 -0300

    Remoção de informações secretas

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,47 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Notification } from './types';
+
+  const allowedOrigins = [
+	'http://localhost:3000',
+	'http://127.0.0.1:3000',
+	'https://negramidiav2.pages.dev',
+	'https://negramidia.net',
+	'https://negramidia.online',
+  ]
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+		//utiliza headers cors para as requisições e trata o preflight
+		const origin = request.headers.get('Origin') ?? '';
+		const corsHeaders = {
+			"Access-Control-Allow-Origin": allowedOrigins.includes(origin) ? origin : '',
+			'Access-Control-Allow-Credentials': 'true',
+			"Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+			'Access-Control-Allow-Headers': '*',
+			"Access-Control-Max-Age": "86400",
+		  };
+		if (request.method === 'OPTIONS') {
+			return new Response(null, {
+			  headers: corsHeaders
+			})
+		}
+
+		try {
+			var notification: Notification = await request.json();
+
+			//Adiciona linha no excel através da API no Azure Functions
+			var resultGSheet = await fetch(env.AzureFunctionURI,{
+				method: 'POST',
+				headers: {
+				  'Content-Type': 'application/json',
+				  'X-Functions-Key': env.AzureFunctionAPIKey
+				},
+				body: JSON.stringify(notification)
+			  });
+
+			return new Response('Mensagem publicada com sucesso.', { status: 200, statusText: 'Sucesso', headers: corsHeaders});
+		}
+		catch (error) {
+			return new Response('Houve um erro ao enviar sua mensagem.', {status: 500, statusText: 'Erro inesperado.', headers: corsHeaders});
+		}
+	}
+}
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export interface Notification{
+	name: string,
+	email: string,
+	phone: string,
+	body: string,
+	subject: string
+}
